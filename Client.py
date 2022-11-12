3d0e002eb4e40248564ece1a92c06643f8cc40c8
BunnyMerz <tguerreirinhos.polo900@gmail.com>
2022-11-12 18:06:55 -0300

    Fixed/Extended client state machine, added util methods on client and made an UI with Pyqt5

--- Client.py
+++ Client.py
@@ -4,26 +4,60 @@ import socket
 from socket import socket as Socket
 
 class Client:
-    def __init__(self, ip='localhost', port=5000):
+    def __init__(
+            self,
+            on_tcp_state_change = lambda new_state : None,
+            on_udp_state_change = lambda new_state : None
+        ):
         self.name = None
-        self.tcp = WSocket(Socket(family=socket.AF_INET, type=socket.SOCK_STREAM))
+        self.tcp = None
         self.last_registry = None
 
         self.udp = Socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
         self.connected_to_udp = None
+        self.connected_to_udp_username = "?"
 
         self.udp.bind(("localhost",0))
-        self.tcp.connect((ip,port))
 
-        self.tcp_state = "unregistered"
-        self.udp_state = "idle"
+        self._tcp_state = "offline"
+        self.on_tcp_state_change = on_tcp_state_change
+        self._udp_state = "idle"
+        self.on_udp_state_change = on_udp_state_change
 
-        thread(self.tcp_listen, ())
         thread(self.udp_listen, ())
 
+
+    def connect_to_server(self, ip, port=5000):
+        try:
+            self.tcp = WSocket(Socket(family=socket.AF_INET, type=socket.SOCK_STREAM))
+            self.tcp.connect((ip,port))
+            thread(self.tcp_listen, ())
+        except Exception as e:
+            print("Failed to connect to server with error:\n", e)
+
+    #### Make pyqt5 easier to handle
+    @property
+    def tcp_state(self):
+        return self._tcp_state
+    @tcp_state.setter
+    def tcp_state(self, _v):
+        self._tcp_state = _v
+        self.on_tcp_state_change()
+
+    @property
+    def udp_state(self):
+        return self._udp_state
+    @udp_state.setter
+    def udp_state(self, _v):
+        self._udp_state = _v
+        self.on_udp_state_change()
+    ################################
+
+    #### Is a function since you can get it off of the socket
     @property
     def udp_address(self):
         return self.udp.getsockname()
+    ###################
 
     def send(self, msg: Message):
         if msg.t == Message.kind("register") and self.tcp_state == "unregistered":
@@ -41,51 +75,91 @@ class Client:
     def udp_send(self, msg: Message, address):
         if msg.t == Message.kind("call_request") and self.udp_state == "idle":
             self.udp_state = "waiting_response"
-
-        if msg.t == Message.kind("end_call") and self.udp_state == "on_call":
+        
+        if msg.t == Message.kind("accept_call") and self.udp_state == "received_request":
+            self.udp_state = "on_call"
+        if msg.t == Message.kind("reject_call") and self.udp_state == "received_request":
+            self.udp_state = "idle"
+            
+        if msg.t == Message.kind("end_call") and self.udp_state in ["on_call", "waiting_response"]:
             self.udp_state = "idle"
             self.connected_to_udp = None
+            self.connected_to_udp_username = "?"
 
+        if address == None:
+            return
         self.udp.sendto(msg.encode(), address)
 
     def tcp_listen(self):
-        msg = None
-        while 1:
-            msg = self.tcp.recv(1024)
+        try:
+            self.tcp_state = "unregistered"
+            msg = None
+            while 1:
+                msg = self.tcp.recv(1024)
 
-            # elif self.tcp_state == "unregistered":
-            #     pass
+                # elif self.tcp_state == "unregistered":
+                #     pass
 
-            if self.tcp_state == "waiting_register" and msg.t == Message.kind("accepted_register"):
-                print("Succesfully registered!")
-                self.tcp_state = "idle"
-                
+                if self.tcp_state == "waiting_register" and msg.t == Message.kind("accepted_register"):
+                    print("Succesfully registered!")
+                    self.tcp_state = "idle"
+                    
 
-            elif self.tcp_state == "waiting_register" and msg.t == Message.kind("declined_register"):
-                print("Some user with that name already exists! Choose another one.")
-                self.name = None
-                self.tcp_state = "unregistered"
-                
+                elif self.tcp_state == "waiting_register" and msg.t == Message.kind("declined_register"):
+                    print("Some user with that name already exists! Choose another one.")
+                    self.name = None
+                    self.tcp_state = "unregistered"
+                    
 
 
-            # elif self.tcp_state == "idle":
-            #     pass
+                # elif self.tcp_state == "idle":
+                #     pass
 
-            elif self.tcp_state == "waiting_registry" and msg.t == Message.kind("registry"):
-                self.last_registry = msg.user
-                print(json.dumps(msg.user))
-                self.tcp_state = "idle"
-                
+                elif self.tcp_state == "waiting_registry" and msg.t == Message.kind("registry"):
+                    self.last_registry = msg.user
+                    self.tcp_state = "idle"
+                elif self.tcp_state == "waiting_registry" and msg.t == Message.kind("not_found"):
+                    self.last_registry = False
+                    self.tcp_state = "idle"
+                    
+            
+                elif self.tcp_state == "disconnecting" and msg.t == Message.kind("accepted_unregister"):
+                    self.tcp.close()
+                    self.tcp_state = "offline"
+                    return
+
+        except Exception as e:
+            print(e)
+            self.tcp_state = "offline"
+
+    def login(self, username):
+        self.send(Message("register", user_name=username, ip=self.udp_address[0], porta=self.udp_address[1]))
+    def logoff(self):
+        self.send(Message("unregister"))
+
+    def call_user(self, user_name):
+        self.last_registry = None
+        self.send(Message("registry", user_name=user_name))
+
+        while self.last_registry == None: ## Will be false if user doesnt exist
+            pass
+        if self.last_registry == False:
+            return
+
+        user_to_call = self.last_registry
+        self.call(user_to_call['ip'], user_to_call['porta'])
+
+    def respond_call_request(self, accept=1):
+        if accept:
+            self.udp_send(Message("accept_call", user_name = self.name), self.connected_to_udp)
+        else:
+            self.udp_send(Message("reject_call",  user_name = self.name), self.connected_to_udp)
         
-            elif self.tcp_state == "disconnecting" and msg.t == Message.kind("accepted_unregister"):
-                self.tcp.close()
-                print("Disconnected!")
-                return
 
     def call(self, ip, porta):
         self.udp_send(Message("call_request", user_name= self.name),(ip,porta))
     def end_call(self):
-        self.udp_send(Message("end_call"),self.connected_to_udp)
+        self.udp_send(Message("end_call"), self.connected_to_udp)
 
     def send_voice(self, voice: bytes):
         self.sendto(Message("voice", voice=voice).encode(), self.connected_to_udp)
@@ -100,24 +174,20 @@ class Client:
             msg = Message.decode(data)
 
             if self.udp_state == "idle" and msg.t == Message.kind("call_request"):
-                r = input(f"User {msg.user_name} is calling! Type 1 to answer and 0 to reject it: ")
-                r = int(r)
-                if r == 1:
-                    self.udp_send(Message("accept_call"), address)
-                    self.connected_to_udp = address
-                    self.udp_state = "on_call"
-                else:
-                    self.udp_send(Message("reject_call"), address)
+                self.connected_to_udp = address
+                self.connected_to_udp_username = msg.user_name
+                self.udp_state = "received_request"
 
-            elif self.udp_state == "on_call" and msg.t == Message.kind("call_request"):
+            elif self.udp_state != "idle" and msg.t == Message.kind("call_request"):
                 self.udp_send(Message("occupied"), address)
 
 
             elif self.udp_state == "waiting_response" and msg.t == Message.kind("accept_call"):
                 self.connected_to_udp = address
+                self.connected_to_udp_username = msg.name
                 self.udp_state = "on_call"
                 
-            elif self.udp_state == "waiting_response" and msg.t == Message.kind("reject_call"):
+            elif self.udp_state == "waiting_response" and msg.t in [Message.kind("reject_call"), Message.kind("occupied")]:
                 self.udp_state = "idle"
         
 
@@ -125,4 +195,5 @@ class Client:
                 self.received_voice(msg.voice)
             elif self.udp_state == "on_call" and msg.t == Message.kind("end_call"):
                 self.connected_to_udp = None
+                self.connected_to_udp_username = None
                 self.udp_state = "idle"
---
+++ ui.py
@@ -0,0 +1,194 @@
+import PyQt5
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import sys
+from Client import Client
+
+class CallPopUp(QDialog):
+    def __init__(self, user_name="An unamed user", *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setWindowTitle("Someone is calling you!")
+
+        QBtn = QDialogButtonBox.Yes | QDialogButtonBox.No
+
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        self.layout = QVBoxLayout()
+        message = QLabel(f"{user_name} is calling you! Accept call?")
+        self.layout.addWidget(message)
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
+
+class Window(QMainWindow):
+    state_change = PyQt5.QtCore.pyqtSignal()
+    def __init__(self):
+        super(Window, self).__init__()
+        self.setGeometry(100,100,600,400)
+        self.setWindowTitle("Voip!")
+        self.console_history = []
+        self.tcp_state = "offline"
+        self.udp_state = "idle"
+
+        self.state_change.connect(self.updated_state)
+        self.client = Client(self.state_change.emit, self.state_change.emit)
+
+        self.cw = QWidget()
+        self.setCentralWidget(self.cw)
+        flo = QFormLayout()
+
+        ### Connection
+        self.ip = QLineEdit()
+        self.ip.setText("localhost")
+        self.connect_btn = QPushButton('Connect to server', self)
+        self.connect_btn.clicked.connect(self.connect)
+        flo.addRow("Ip: (0.0.0.0)", self.ip)
+        flo.addRow(self.connect_btn)
+        ###################
+
+        ### Login
+        self.user_name = QLineEdit()
+        self.user_name.setText("Bruno")
+        self.login_btn = QPushButton('Login', self)
+        self.login_btn.clicked.connect(self.login)
+        flo.addRow("Login as:", self.user_name)
+        flo.addRow(self.login_btn)
+        ###################
+
+        ### TCP server stuff
+        self.server_status = QLabel(self)
+        self.server_status.setGeometry(20,20,100,100)
+        self.server_status.setText("[offline]")  
+        flo.addRow("Server stauts: ", self.server_status)
+
+        self.user_to_call = QLineEdit()
+        flo.addRow("User name:", self.user_to_call)
+        ###################
+
+        ### Action bar
+        hbox = QHBoxLayout()
+        self.dc = QPushButton("Disconnect (from server)")
+        self.dc.clicked.connect(self.disconnect)
+        hbox.addWidget(self.dc)
+
+
+        self.call_btn = QPushButton('Call user')
+        self.call_btn.clicked.connect(self.call)
+        hbox.addWidget(self.call_btn, 1)
+
+        self.ec = QPushButton("End Call")
+        self.ec.clicked.connect(self.end_call)
+        hbox.addWidget(self.ec, 2)
+
+        flo.addRow(hbox)
+        ###################
+
+        self.cw.setLayout(flo)
+
+        self.updated_state()
+
+    def updated_state(self):
+        self.tcp_state = self.client.tcp_state
+        self.udp_state = self.client.udp_state
+        self.server_status.setText(f"[tcp:{self.tcp_state}, udp:{self.udp_state}]")
+
+        ##### UDP ######
+        if self.udp_state == "idle":
+            self.ec.setDisabled(1)
+            self.call_btn.setDisabled(0)
+        if self.udp_state == "waiting_response":
+            self.ec.setDisabled(0)
+            self.call_btn.setDisabled(1)
+        if self.udp_state == "received_request":
+            self.client.respond_call_request(self.call_request_pop_up())
+            self.ec.setDisabled(1)
+            self.call_btn.setDisabled(1)
+        if self.udp_state == "on_call":
+            self.ec.setDisabled(0)
+            self.call_btn.setDisabled(1)
+
+
+        ###### TCP #######
+        if self.tcp_state == "offline":
+            ## Connect to server enabled
+            self.ip.setDisabled(0)
+            self.connect_btn.setDisabled(0)
+
+            ## Set name disbled
+            self.user_name.setDisabled(1)
+            self.login_btn.setDisabled(1)
+
+            ## Call disabled
+            self.dc.setDisabled(1)
+            self.call_btn.setDisabled(1)
+            self.user_to_call.setDisabled(1)
+            
+        elif self.tcp_state == "unregistered":
+            ## Connect to server enabled
+            self.ip.setDisabled(1)
+            self.connect_btn.setDisabled(1)
+
+            ## Set name disbled
+            self.user_name.setDisabled(0)
+            self.login_btn.setDisabled(0)
+
+            ## Call disabled
+            self.dc.setDisabled(1)
+            self.call_btn.setDisabled(1)
+            self.user_to_call.setDisabled(1)
+            
+        elif self.tcp_state == "waiting_register":
+            ## Connect to server enabled
+            self.ip.setDisabled(1)
+            self.connect_btn.setDisabled(1)
+
+            ## Set name disbled
+            self.user_name.setDisabled(1)
+            self.login_btn.setDisabled(1)
+
+            ## Call disabled
+            self.dc.setDisabled(1)
+            self.call_btn.setDisabled(1)
+            self.user_to_call.setDisabled(1)
+            
+        elif self.tcp_state == "idle":
+            ## Connect to server enabled
+            self.ip.setDisabled(1)
+            self.connect_btn.setDisabled(1)
+
+            ## Set name disbled
+            self.user_name.setDisabled(1)
+            self.login_btn.setDisabled(1)
+
+            ## Call disabled
+            self.dc.setDisabled(0)
+            self.call_btn.setDisabled(0)
+            self.user_to_call.setDisabled(0)
+        
+
+
+    def connect(self):
+        self.client.connect_to_server(self.ip.text())
+    def disconnect(self):
+        self.client.logoff()
+
+    def login(self):
+        self.client.login(self.user_name.text())
+
+    def call(self):
+        self.client.call_user(self.user_to_call.text())
+    def end_call(self):
+        self.client.end_call()
+   
+    def call_request_pop_up(self):
+        p = CallPopUp(self.client.name,self)
+        return p.exec_()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
