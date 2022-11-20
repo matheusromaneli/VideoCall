f4b63322f7c45305d7c433b71d252557aa3480f6
Rafael Kanazawa <rafael.kanazawa@hotmail.com>
2022-11-20 13:10:39 -0300

    Feat/Name_List por Rafael Kanazawa: Retirando definicao de Callable

--- util/thread.py
+++ util/thread.py
@@ -1,13 +1,5 @@
 from threading import Thread
 
-class Callable():
-    def __init__(self, func, **kwargs) -> None:            
-        self.func = func
-        self.kwargs = kwargs
-
-    def __call__(self, *args, **kwds):
-        return self.func(*args, **self.kwargs, **kwds)
-
 def thread(fn, args):
     t = Thread(target=fn, args=args, daemon=True)
     t.start()
